e1c1ab908f244a9760d639b4c6711ea36c159b38
budopest <abdo.mohamed5111995@gmail.com>
2018-04-04 02:00:36 +0200

    Testchanges

--- app/src/main/java/com/gp/eece2019/wecare/MainActivity.java
+++ app/src/main/java/com/gp/eece2019/wecare/MainActivity.java
@@ -28,7 +28,7 @@ public class MainActivity extends AppCompatActivity
 
     USERsqllitehandler usql;
     //DatabaseHelper bybass;
-
+    //testgithub
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
