7239a449620b554351390bd49f5111fd6ccc54d6
budopest <abdo.mohamed5111995@gmail.com>
2018-04-21 00:10:26 +0200

    change the ip one time only in Sip variable

--- app/src/main/java/com/gp/eece2019/wecare/login/IPSTRING.java
+++ app/src/main/java/com/gp/eece2019/wecare/login/IPSTRING.java
@@ -7,10 +7,11 @@ package com.gp.eece2019.wecare.login;
 public class IPSTRING {
 
 
-    private final String login_url = "http://192.168.1.73/logintest.php";
-    private final String signup_url = "http://192.168.1.73/signuptest.php";
-    private final String medicine_url = "http://192.168.1.121/medicines.php";
-    private final String measures_url = "http://192.168.1.121/readm.php";
+    private final String Sip          = "http://192.168.1.121/";
+    private final String login_url    = Sip+"logintest.php";
+    private final String signup_url   = Sip+"signuptest.php";
+    private final String medicine_url = Sip+"medicines.php";
+    private final String measures_url = Sip+"readm.php";
 
     public String Getlogin()
     {
